40267da8723ddff2b443a0dcbcae96c9090d3e53
e <eris@jpegzilla.com>
2023-03-05 17:45:55 -0500

    added configuration option for region

--- lib/constants/constants.rb
+++ lib/constants/constants.rb
@@ -1,10 +1,28 @@
 # frozen_string_literal: true
 
+REGION = ENV['BDO_AP_REGION'] || 'na'
+
+REGION_DOMAINS = {
+  na: 'na-trade.naeu.playblackdesert.com',
+  eu: 'eu-trade.naeu.playblackdesert.com',
+  eu_console: 'eu-trade.console.playblackdesert.com',
+  na_console: 'na-trade.console.playblackdesert.com',
+  asia_console: 'asia-trade.console.playblackdesert.com',
+  sea: 'trade.sea.playblackdesert.com',
+  mena: 'trade.tr.playblackdesert.com',
+  kr: 'trade.kr.playblackdesert.com',
+  ru: 'trade.ru.playblackdesert.com',
+  jp: 'trade.jp.playblackdesert.com',
+  th: 'trade.th.playblackdesert.com',
+  tw: 'trade.tw.playblackdesert.com',
+  sa: 'blackdesert-tradeweb.playredfox.com'
+}.freeze
+
 module Constants
   COOKIE =
     '__RequestVerificationToken=0q_pyZ8OMkALLxxv_pj6ty10hXKTDq3Sl_1NAbV5WYhUAbNvcurT6GwSUDmBRnaZVyEmd0lcTlR3I6X7_cXkAwH-nGQG_G2E6MQZPNcfNd41'
   RVT = 'MGf54RSTVQkYrPo4Dvaf9IOuSilYwQIzWfdBI4PhFrV1l1o_e0BRmf78J6I7pVn_gcqj5DAv-G3MfFiWoUqFE3R62-Kxdcm4CgkRGd7oq7Y1'
-  ROOT_URL = 'https://na-trade.naeu.playblackdesert.com/Home'
+  ROOT_URL = "https://#{REGION_DOMAINS[REGION.to_sym]}/Home"
   WORLD_MARKET_LIST = '/GetWorldMarketList'
   MARKET_SUB_LIST = '/GetWorldMarketSubList'
   MARKET_SEARCH_LIST = '/GetWorldMarketSearchList'
